e983d74914e2000e945720220406814f5038dbc9
krant <krant@DESKTOP-4N9DCJ0>
2020-05-29 14:48:27 -0700

    commmit 2

--- Git-Java-Projects/src/practice.java
+++ Git-Java-Projects/src/practice.java
@@ -1,4 +1,19 @@
-
+import java.util.Scanner;
 public class practice {
-
+	public static void main(String args[]) {
+		Scanner x = new Scanner(System.in);
+		 
+		double a;
+		double b;
+		double e;
+		
+		System.out.println("a value is : ");
+		a= x.nextDouble();
+		System.out.println("b value is : ");
+		b= x.nextDouble();
+		
+		e=a/b;
+		System.out.println("result e is : " + e);	 
+		
+	}
 }
