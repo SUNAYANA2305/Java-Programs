df143d09556730bb44842d01c3b0227837deb0e1
krant <krant@DESKTOP-4N9DCJ0>
2020-06-02 13:03:46 -0500

    new class

---
+++ Git-Java-Projects/src/ruhi.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ruhi {
+	public static void main(String args[]) {
+		
+		Scanner y = new Scanner(System.in);
+		
+		int a;
+		int b;
+		int c;
+		
+		System.out.println("a value is : ");
+		a = y.nextInt();
+		
+		System.out.println("b value is : ");
+		b = y.nextInt();
+		
+		c=a/b;
+		
+		System.out.println(" result c is : " + c);
+		
+		
+		
+	}
+
+}
