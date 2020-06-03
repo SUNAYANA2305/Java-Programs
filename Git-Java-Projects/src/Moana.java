1c4543f37a3dcef82a1ddcb86e356c74850f5911
krant <krant@DESKTOP-4N9DCJ0>
2020-06-03 11:52:10 -0500

    if else

---
+++ Git-Java-Projects/src/Moana.java
@@ -0,0 +1,16 @@
+
+public class Moana {
+	public static void main(String args[]) {
+		int computers = 50;
+		int unix = 45;
+		int networking = 55;
+		
+		if(computers > 40 && unix >= 55 && networking > 40) {
+			System.out.println("pass");
+		}else {
+			System.out.println("fail");
+			
+		} 
+	}
+
+}
