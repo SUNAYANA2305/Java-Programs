b83aa7e528eec1224c90699bdfc9d5775119e280
krant <krant@DESKTOP-4N9DCJ0>
2020-06-03 12:38:14 -0500

    switch statement

---
+++ Git-Java-Projects/src/croods.java
@@ -0,0 +1,27 @@
+
+public class croods {
+	public static void main(String args[]) {
+			
+			int age =18;
+			
+			switch(age) {
+			case 18:
+				System.out.println(" you can vote");
+				break;
+			case 25:
+				System.out.println(" you can marry ");
+				break;
+			case 60:
+				System.out.println(" you can retire now");
+				break;
+			default:
+				System.out.println(" sorry! your not eligible");
+				
+					
+			}
+		
+			
+		}
+
+	}
+
